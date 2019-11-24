55ecd44274061071bb973c2132be7d255a11b13e
Aulang <aulang@qq.com>
2019-11-24 23:02:34 +0800

    初始化框架代码

---
+++ trustsign/src/test/java/net/aulang/pdf/trustsign/TrustSignApplicationTests.java
@@ -0,0 +1,13 @@
+package net.aulang.pdf.trustsign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrustSignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
